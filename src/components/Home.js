ac7cbc974e6712a34f8eab9990104965fab535a0
Neeraj0406 <neerajsingh0406@gmail.com>
2022-01-18 01:33:26 +0530

    minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { NavLink, Link } from 'react-router-dom';
+import {  Link } from 'react-router-dom';
 import axios from 'axios';
 import { useState } from 'react/cjs/react.development';
 import "./Home.css"
